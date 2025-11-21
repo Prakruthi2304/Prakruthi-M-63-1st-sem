7b5b3030a5afd4692223625adc5855e269563e71
dharani <dharanib1317@gmail.com>
2025-11-21 15:29:15 +0530

    day 5 code pushed

---
+++ day5/12_forloop1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("numbers from 1 to 10:\n");
+    for (int i = 1; i <= 100; i++)
+        if (1 % 2 == 0)
+        {
+            printf("%d ", i);
+        }
+}
