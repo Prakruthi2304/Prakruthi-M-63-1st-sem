0866c8098accef7b057e4ea560b773f7dc2b390b
dharani <dharanib1317@gmail.com>
2025-11-27 14:50:05 +0530

    day9 code pushed

---
+++ day9/4_bubble.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int arr[50], pos = 0, ele = 0, size = 0;
+    printf("enter a size of array: \n");
+    scanf("%d", &size);
+    if (size > 50)
+    {
+        printf("array out of bound\n");
+    }
+    else
+    {
+        // to insert
+        printf("enter elements: \n");
+        for (int i = 0; i < size; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j <= size - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printf("array elements: \n");
+        for (int i = 0; i < size; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+}
